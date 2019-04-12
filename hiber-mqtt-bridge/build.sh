c488ed851692ce2a98e9352c6c842e8dd93f87e3
Delano Keuter <delanokeuter@gmail.com>
2019-04-12 13:26:12 +0200

    updated build script and added gitignore

--- hiber-mqtt-bridge/build.sh
+++ hiber-mqtt-bridge/build.sh
@@ -7,5 +7,7 @@ make all
 # Copy config file
 cp ./config.json ./build/config.json
 
+mv ./build/hummingbird-hiber-bridge-linux-arm ./build/hiber-bridge
+
 # Update RPi
 scp -r build pi@rpi:/home/pi/hiber
