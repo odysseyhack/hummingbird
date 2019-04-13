0c5182235b66ccb6c83d740b2b448f3bce539b45
Jelle Licht <jlicht@fsfe.org>
2019-04-14 00:09:37 +0200

    Add demo constants.

---
+++ go-stuff/src/github.com/milvum/hummingbird/demo/constants.go
@@ -0,0 +1,17 @@
+package demo
+
+import (
+	cipher "github.com/skycoin/skywire/pkg/cipher"
+)
+
+func publicKey(hex string) cipher.PubKey {
+	s := cipher.PubKey{}
+	s.Set(hex)
+	return s
+}
+
+var publicKeys = map[string]cipher.PubKey {
+	"host1" : publicKey("0327f6951690898c10a02f793e367b6349492bfa266b696a81005cb62bf0f32e52"),
+	"host2" : publicKey("03477919f99e6d4ab2d0f002fd2313562cb0678bbd2e980c32a2c7e4a2483dab5e"),
+}
+
