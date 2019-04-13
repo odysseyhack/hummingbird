1bd59b5bd73713a98fc6dce4788ea9bdd857d3f1
Jelle Licht <jlicht@fsfe.org>
2019-04-14 00:10:37 +0200

    go-fmt

--- go-stuff/src/github.com/milvum/hummingbird/demo/constants.go
+++ go-stuff/src/github.com/milvum/hummingbird/demo/constants.go
@@ -10,8 +10,7 @@ func publicKey(hex string) cipher.PubKey {
 	return s
 }
 
-var publicKeys = map[string]cipher.PubKey {
-	"host1" : publicKey("0327f6951690898c10a02f793e367b6349492bfa266b696a81005cb62bf0f32e52"),
-	"host2" : publicKey("03477919f99e6d4ab2d0f002fd2313562cb0678bbd2e980c32a2c7e4a2483dab5e"),
+var publicKeys = map[string]cipher.PubKey{
+	"host1": publicKey("0327f6951690898c10a02f793e367b6349492bfa266b696a81005cb62bf0f32e52"),
+	"host2": publicKey("03477919f99e6d4ab2d0f002fd2313562cb0678bbd2e980c32a2c7e4a2483dab5e"),
 }
-
