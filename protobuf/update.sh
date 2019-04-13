515bcc7252453760ce54d28915c5b61cf7366f01
Jelle Licht <jlicht@fsfe.org>
2019-04-13 12:13:53 +0200

    Fix up grpc generation for golang.

--- protobuf/update.sh
+++ protobuf/update.sh
@@ -4,6 +4,6 @@ IFS=$'\n\t'
 
 BASEDIR=$(dirname "$0")
 mkdir -p ../app/lib/generated
-protoc --go_out=grpc:../go-stuff/src/github.com/milvum/hummingbird/proto \
+protoc --go_out=plugins=grpc:../go-stuff/src/github.com/milvum/hummingbird/proto \
        --dart_out=grpc:../app/lib/generated \
        -I$BASEDIR $BASEDIR/hummingbird.proto
