772425402f723d458f18274484ad543c5e54b736
Delano Keuter <delanokeuter@gmail.com>
2019-04-13 16:07:26 +0200

    added basic dashboard ui test

--- dashboard/test/unit/jest.conf.js
+++ dashboard/test/unit/jest.conf.js
@@ -7,15 +7,18 @@ module.exports = {
     'json',
     'vue',
   ],
-  moduleNameMapper: {
-    '^@/(.*)$': '<rootDir>/src/$1',
-  },
   transform: {
     '^.+\\.js$': '<rootDir>/node_modules/babel-jest',
     '.*\\.(vue)$': '<rootDir>/node_modules/vue-jest',
   },
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
   testPathIgnorePatterns: [
     '<rootDir>/test/e2e',
+  ],
+  testMatch: [
+    '<rootDir>/(test/unit/specs/**.spec.(js|jsx|ts|tsx)|**/__tests__/*.(js|jsx|ts|tsx))'
   ],
   snapshotSerializers: ['<rootDir>/node_modules/jest-serializer-vue'],
   setupFiles: ['<rootDir>/test/unit/setup'],
